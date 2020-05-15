663965b56320c1fc11937a3f91aeb5399192de93
Amit Kakade <amitkakade115@gmail.com>
2020-05-16 00:24:15 +0530

    #Amit Kakade 16th May 2020 HashSet Introduction: Covered almost all properties of HashSet.

---
+++ src/set/hashset/HashSetMethodsIntro.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package set.hashset;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Amit Kakade
+ */
+public class HashSetMethodsIntro {
+    public static void main(String[] args) {
+        HashSet h = new HashSet();
+        h.add("Z");
+        h.add("A");
+        h.add("C");
+        h.add(null);
+        h.add(10);
+        System.out.println(h.add("Z"));
+        System.out.println(h);
+    }
+}
