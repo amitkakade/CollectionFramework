e9593884ded7347286c478897a1c618557a67f0a
amit.kakade <amitkakade115@gmail.com>
2020-05-31 14:29:45 +0530

    # Amit Kakade 31th May 2020 Changing packages names

--- src/collection/queue/deque/linkedlist/LinkedListIntro.java
+++ src/collection/queue/deque/linkedlist/LinkedListIntro.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package collection.deque.linkedlist;
+package collection.queue.deque.linkedlist;
 
 import java.util.Deque;
 import java.util.LinkedList;
